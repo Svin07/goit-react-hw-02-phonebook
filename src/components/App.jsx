df12b7593c75d0ad106e5e7d944b5c746fad7e64
Svin07 <svin07@ukr.net>
2023-09-06 14:21:44 +0300

    commit_01

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,12 @@
 export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+  return <div>ДР 2 завдання 2</div>;
 };
+
+// {
+//         height: '100vh',
+//         display: 'flex',
+//         justifyContent: 'center',
+//         alignItems: 'center',
+//         fontSize: 40,
+//         color: '#010101',
+//       }
